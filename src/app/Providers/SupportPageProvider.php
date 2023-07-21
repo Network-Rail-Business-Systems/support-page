e9ade46463a44c467404eb47fd0b852b42781ece
Sim Roberts <simran.roberts@networkrail.co.uk>
2023-07-21 10:33:20 +0100

    removed register from provider, added boot publishes and corresponding paths

--- src/app/Providers/SupportPageProvider.php
+++ src/app/Providers/SupportPageProvider.php
@@ -8,9 +8,18 @@ class SupportPageProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        $this->bootPublishes();
         $this->bootRoutes();
         $this->bootViews();
+    }
 
+    protected function bootPublishes(): void
+    {
+        $this->publishes([
+            __DIR__.'/../../resources/views/errors' => resource_path('views/errors'),
+            __DIR__.'/../../resources/views/components' => resource_path('views/components'),
+            __DIR__.'/../../resources/views/support' => resource_path('views/support'),
+        ]);
     }
 
     protected function bootRoutes(): void
