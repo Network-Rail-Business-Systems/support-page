9c0f97b2c495a66dd9b27f7506e3fcf228174adb
Sim Roberts <simran.roberts@networkrail.co.uk>
2023-07-28 10:22:41 +0100

    changed namespace on bootviews, changed file paths and added test.php to run and push out paths for confirmation

--- src/resources/views/errors/support.blade.php
+++
@@ -1,6 +0,0 @@
-<x-govuk::p>
-    <x-govuk::a href="{{ route('support') }}" target="_blank">
-        Contact the support team <x-govuk::hidden>(opens in a new tab)</x-govuk::hidden>
-    </x-govuk::a>
-    if you require further assistance.
-</x-govuk::p>
