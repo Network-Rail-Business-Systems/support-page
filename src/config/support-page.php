1096cd85c3810d38f41681a9f90d6a0b739e3cc9
Sim Roberts <simran.roberts@networkrail.co.uk>
2023-09-08 11:40:20 +0100

    added support detail class to config

--- src/config/support-page.php
+++ src/config/support-page.php
@@ -1,7 +1,9 @@
 <?php
 
+use NetworkRailBusinessSystems\SupportPage\Models\SupportDetail;
+
 return [
     'user_model' => env('USER_MODEL', '\App\Models\User'),
-    'support_detail_model' => env('SUPPORT_DETAIL_MODEL', '\NetworkRailBusinessSystems\SupportPage\Models\SupportDetail'),
+    'support_detail_model' => env('SUPPORT_DETAIL_MODEL', SupportDetail::class),
     'update_permissions' => env('UPDATE_PERMISSIONS', '\App\Console\Commands\UpdatePermissions'),
 ];
