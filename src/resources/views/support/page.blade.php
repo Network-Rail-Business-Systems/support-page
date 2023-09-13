b699ac9e13426915da5f5dd685ef56e2208192c2
Sim Roberts <simran.roberts@networkrail.co.uk>
2023-09-13 10:48:31 +0100

    added sections to the support page

--- src/resources/views/support/page.blade.php
+++ src/resources/views/support/page.blade.php
@@ -1,24 +1,36 @@
 @php use NetworkRailBusinessSystems\SupportPage\Forms\SupportDetail\Questions\TypeQuestion; @endphp
+<head>
 <body>
-
-<div class="title">
-    <div class="container is-fullhd">
-        <h2> Support</h2>
-    </div>
-</div>
-
-<div>
-    @foreach($groups as $type => $group)
-        <x-support-page::support-group
-                description="{{ TypeQuestion::DESCRIPTIONS[$type] }}"
-                :group="$group"
-                title="{{ TypeQuestion::OPTIONS[$type] }}"
-        />
-    @endforeach
-</div>
-
-<div style="float:right;">
-    <h2>System details<div>
+<section>
+    <aside>
+        <div style="float: right;">
+            <h2>System details</h2>
             <x-support-page::support-summary-list :list="$list"/>
         </div>
+    </aside>
+</section>
+<section>
+    <header>
+        <div class="container is-fullhd">
+            <h2> Support</h2>
+        </div>
+    </header>
+</section>
+
+<div>
+    <section>
+        @foreach($groups as $type => $group)
+            <x-support-page::support-group
+                    description="{{ TypeQuestion::DESCRIPTIONS[$type] }}"
+                    :group="$group"
+                    title="{{ TypeQuestion::OPTIONS[$type] }}"
+            />
+        @endforeach
+    </section>
+</div>
 </body>
+</head>
+
+
+
+
